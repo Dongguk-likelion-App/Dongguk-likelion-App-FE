a15e04fc65ed4de3606f6a1d849139aa5a005ded
taejun0 <ohtaetaejun@gmail.com>
2025-11-26 16:39:21 +0900

    [Feature] RadioGroup 컴포넌트 column prop 추가
    - column prop 추가로 세로 배치 지원
    - 기본값은 가로 배치 (row)
    - SignupScreen에서 트랙 및 현재 신분 선택에 활용

--- src/storybook/stories/design-system/components/RadioGroup/RadioGroup.tsx
+++ src/storybook/stories/design-system/components/RadioGroup/RadioGroup.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Wrapper, Option, OuterCircle, InnerCircle, Label } from './RadioGroup.styles';
+import {
+  Wrapper,
+  Option,
+  OuterCircle,
+  InnerCircle,
+  Label,
+} from './RadioGroup.styles';
 import type { RadioGroupProps } from './RadioGroup.types';
 
-export default function RadioGroup({ options, selected, onSelect }: RadioGroupProps) {
+export default function RadioGroup({
+  options,
+  selected,
+  onSelect,
+  column = false,
+}: RadioGroupProps) {
   return (
-    <Wrapper>
+    <Wrapper column={column}>
       {options.map((option, i) => (
         <Option key={option} onPress={() => onSelect(i)}>
           <OuterCircle>{selected === i && <InnerCircle />}</OuterCircle>
